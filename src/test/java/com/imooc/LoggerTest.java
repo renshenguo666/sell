7480e395f0a2c41b3b758b34856da4129670bf11
liuyang <389652131@qq.com>
2018-07-01 23:22:45 +0800

    logback config

---
+++ src/test/java/com/imooc/LoggerTest.java
@@ -0,0 +1,26 @@
+package com.imooc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+//    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLoger(){
+        String name = "jack";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name:{};password:{}",name,password);
+        log.warn("warn...");
+        log.error("error...");
+    }
+}
