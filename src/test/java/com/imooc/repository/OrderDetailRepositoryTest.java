3b48e659fbdc1dc74f362339bc7bc39a1512714f
liuyang <389652131@qq.com>
2018-07-17 23:07:58 +0800

    20180717

--- src/test/java/com/imooc/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/imooc/repository/OrderDetailRepositoryTest.java
@@ -1,12 +1,44 @@
 package com.imooc.repository;
 
+import com.imooc.dataobject.OrderDetail;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class OrderDetailRepositoryTest {
+
+    @Autowired
+    private OrderDetailRepository repository;
+
+    @Test
+    public void saveTest(){
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setDetailId("100001");
+        orderDetail.setOrderId("123456");
+        orderDetail.setProductIcon("xxx/2.jpg");
+        orderDetail.setProductId("111112");
+        orderDetail.setProductName("可乐");
+        orderDetail.setProductPrice(new BigDecimal(3.5));
+        orderDetail.setProductQuantity(2);
+
+        OrderDetail result = repository.save(orderDetail);
+        Assert.assertNotNull(result);
+    }
+
 
     @Test
     public void findByOrderId() {
+        List<OrderDetail> orders = repository.findByOrderId("123456");
+        Assert.assertNotEquals(0,orders.size());
     }
 }
--- src/test/java/com/imooc/repository/OrderMasterRepositoryTest.java
+++ src/test/java/com/imooc/repository/OrderMasterRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.imooc.repository;
 
+import com.imooc.dataobject.OrderMaster;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
@@ -14,12 +20,27 @@ public class OrderMasterRepositoryTest {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
+    private final String OPEN_ID = "110110";
     @Test
     public void saveTest(){
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("123457");
+        orderMaster.setBuyerName("tom");
+        orderMaster.setBuyer_address("北京");
+        orderMaster.setBuyerPhone("137xxxxxxxx");
+        orderMaster.setBuyerOpenid(OPEN_ID);
+        orderMaster.setOrderAmount(new BigDecimal(15));
 
+        OrderMaster result = orderMasterRepository.save(orderMaster);
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void findByBuyerOpenid() {
+        PageRequest pageRequest = new PageRequest(0,3);
+
+        Page<OrderMaster> result = orderMasterRepository.findByBuyerOpenid(OPEN_ID, pageRequest);
+
+        Assert.assertNotEquals(0,result.getTotalElements());
     }
 }
